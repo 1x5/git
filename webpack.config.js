26109a15f4b807577cfd4433f25624373cdb89dd
an <61973725+1x5@users.noreply.github.com>
2025-03-01 18:02:58 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -33,71 +32,12 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html'
-    }),
-    new webpack.DefinePlugin({
-      'process.env.REACT_APP_API_URL': JSON.stringify(process.env.REACT_APP_API_URL || 'https://api.code1x5.ru/api')
     })
   ],
   devServer: {
     port: 3000,
-    historyApiFallback: true,
     proxy: {
-      '/api': {
-        target: 'http://localhost:8081',
-        secure: false,
-        changeOrigin: true
-      }
+      '/api': 'http://localhost:8081'
     }
   }
 };
-
-
-
-
-
-
-
-
-// const path = require('path');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-// module.exports = {
-//   entry: './src/index.js',
-//   output: {
-//     path: path.resolve(__dirname, 'static'),
-//     filename: 'bundle.js',
-//     publicPath: '/'
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.(js|jsx)$/,
-//         exclude: /node_modules/,
-//         use: {
-//           loader: 'babel-loader',
-//           options: {
-//             presets: ['@babel/preset-env', '@babel/preset-react']
-//           }
-//         }
-//       },
-//       {
-//         test: /\.css$/,
-//         use: ['style-loader', 'css-loader']
-//       }
-//     ]
-//   },
-//   resolve: {
-//     extensions: ['.js', '.jsx']
-//   },
-//   plugins: [
-//     new HtmlWebpackPlugin({
-//       template: './public/index.html'
-//     })
-//   ],
-//   devServer: {
-//     port: 3000,
-//     proxy: {
-//       '/api': 'http://localhost:8081'
-//     }
-//   }
-// };
